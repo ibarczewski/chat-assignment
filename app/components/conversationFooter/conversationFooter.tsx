9e19966639ceadbe45944ea73e322e78d6e61332
Ian Barczewski <ibarczewski@gmail.com>
2024-05-14 17:39:35 -0700

    main - added functionality for mobile sending - ib

--- app/components/conversationFooter/conversationFooter.tsx
+++ app/components/conversationFooter/conversationFooter.tsx
@@ -5,26 +5,39 @@ import GifButton from '../gifButton/gifButton';
 import styles from './conversationFooter.module.scss';
 import { useState } from 'react';
 import GifPrompt from '../gifPrompt/gifPrompt';
+import SendButton from '../sendButton/sendButton';
 
 const ConversationFooter = () => {
+    const [message, setMessage] = useState('');
     const [showGifPrompt, setShowGifPrompt] = useState(false);
     const { addMessage } = useMessageStore(
         (state) => state,
       )
 
-    const handleKeyDown = (e: any) => {
-        
-        if (e.key === 'Enter') {
+    const handleKeyDown = (e: any) => {       
+        if (e.key === 'Enter' && e.target.value.trim() !== '') {
             addMessage(2, { message: e.target.value, isGif: false})
-            e.target.value = '';
-        }
+            setMessage('');
+        } 
+    }
+
+    const handleOnChange = (e: any) => {
+        setMessage(e.target.value);
+    }
+
+    const handleSendMessage = () => {
+        addMessage(2, {message, isGif: false});
+        setMessage('');
     }
 
     return <div>
             <div className={styles.container}>
                 <div className={styles.wrapper}>
-                    <input className={styles.textInput} type='text' placeholder='Message Peter' onKeyDown={handleKeyDown}></input>
-                    <GifButton handleTap={() => setShowGifPrompt(!showGifPrompt)} />
+                    <input className={styles.textInput} type='text' placeholder='Message Peter' value={message} onChange={handleOnChange} onKeyDown={handleKeyDown}></input>
+                    <div className={styles.actions}>
+                        <SendButton handleTap={() => handleSendMessage()} />
+                        <GifButton handleTap={() => setShowGifPrompt(!showGifPrompt)} />
+                    </div>
                 </div>
                 {
                     showGifPrompt && <div><GifPrompt handleSentGif={() => setShowGifPrompt(false)} /></div>
